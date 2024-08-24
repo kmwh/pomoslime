6dd4f3b5a93d95b22f41e2d1503dfadfa0ca61e5
KMWH <kmwh030@gmail.com>
2024-08-24 16:32:45 +0900

    FEAT: Focus Calender 틀 구현

--- lib/screens/calender_screen.dart
+++ lib/screens/calender_screen.dart
@@ -1,26 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
+import 'package:pomoslime/widgets/calender/calender_tile.dart';
 
-class CalenderScreen extends StatelessWidget {
+class CalenderScreen extends StatefulWidget {
   const CalenderScreen({super.key});
+
+  @override
+  State<CalenderScreen> createState() => _CalenderScreenState();
+}
+
+class _CalenderScreenState extends State<CalenderScreen> {
+  List focusTimes = [
+    ["전체", 456789],
+    ["오늘", 12],
+    ["이번 주", 123],
+    ["이번 달", 1234],
+    ["이번 년도", 12345],
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(
-        vertical: 50,
+        vertical: 60,
         horizontal: 32,
       ),
       child: SingleChildScrollView(
         child: Column(
           children: [
+            const Center(
+              child: Text(
+                "Focus Calender",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
             HeatMap(
               datasets: {
                 DateTime(2024, 8, 6): 1,
                 DateTime(2024, 8, 7): 2,
                 DateTime(2024, 8, 8): 3,
                 DateTime(2024, 8, 9): 4,
-                DateTime(2024, 8, 13): 5,
+                DateTime(2024, 8, 24): 5,
               },
               defaultColor: const Color.fromRGBO(129, 129, 129, 0.23),
               colorMode: ColorMode.opacity,
@@ -31,6 +55,48 @@ class CalenderScreen extends StatelessWidget {
                 1: Color.fromARGB(255, 65, 182, 29),
               },
               size: 21,
+            ),
+            const SizedBox(height: 10),
+            ...focusTimes.map((focusTime) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 6),
+                child: CalenderTile(
+                  period: focusTime[0],
+                  minutes: focusTime[1],
+                ),
+              );
+            }),
+            const SizedBox(
+              height: 18,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 7, horizontal: 8),
+                    child: Image.asset(
+                      "assets/images/download.png",
+                      height: 28,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 7, horizontal: 8),
+                    child: Image.asset(
+                      "assets/images/upload.png",
+                      height: 28,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
---
+++ lib/widgets/calender/calender_tile.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class CalenderTile extends StatelessWidget {
+  final String period;
+  final int minutes;
+
+  const CalenderTile({
+    super.key,
+    required this.period,
+    required this.minutes,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surfaceBright,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 21,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              period,
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            Text(
+              "$minutes min",
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
